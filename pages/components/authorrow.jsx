9215d947b21c21691a64b12b913aeefc0d9215c3
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2022-06-14 11:04:26 +0530

    author row

---
+++ pages/components/authorrow.jsx
@@ -0,0 +1,66 @@
+
+
+
+export const AuthorRow = ()=>{
+
+    return(
+            <div className="flex flex-row h-[50px] p-3 space-x-2 border-t-2 border-b-2 m-0 mt-0">
+                <div  className=" w-[50px] h-full ">
+                    <input className="w-4 h-4" type='checkbox'></input>
+                
+                    
+                </div>
+                
+                <div className="w-[60px] items-center content-center h-full ">
+                    <h4>Author</h4>
+                
+
+                </div>
+                
+                <div className=" w-[190px] items-center content-center h-full ">
+                    <h4>Article</h4>
+
+                </div>
+                
+                <div className="w-[230px] items-center content-center h-full ">
+                    <h4>Date</h4>
+
+                </div>
+                
+                <div className="w-[300px] items-center content-center h-full ">
+                    <h4>Author</h4>
+
+                </div>
+                
+                <div className="w-[160px] items-center content-center h-full">
+                    <h4 className="relative left-3" >Topic</h4>
+
+                </div>
+                
+                <div className="w-[80px] items-center content-center h-full">
+                    <h4>Edit</h4>
+
+                </div>
+                
+                <div className="w-[80px] items-center content-center h-full">
+                    <h4>Delete</h4>
+
+                </div>
+            
+            
+            
+            
+            
+            
+            
+            </div>
+
+
+
+
+    )
+
+
+
+
+}
