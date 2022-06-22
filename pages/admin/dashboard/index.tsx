febad9f30d718237a027f71ee9b1dbe6a11148fe
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2022-06-22 19:24:21 +0530

    added pie chart

--- pages/admin/dashboard/index.tsx
+++ pages/admin/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import type {NextPage} from 'next'
-import { useRef , useEffect } from 'react';
+import { useRef , useEffect, useState } from 'react';
 import {RadialChart} from 'react-vis';
 
 
@@ -13,15 +13,21 @@ import axios from 'axios';
 
 
 
-const Home:NextPage=()=>{
+const Home:NextPage=({authors , articles , topics , topicwise})=>{
     
 
     
-    const myData = [ {angle: 1 }, {angle: 2, label: 'Super Custom label', subLabel: 'With annotation'}, {angle: 5, label: 'Alt Label'}, {angle: 3}, {angle: 5,  subLabel: 'Sub Label only', className: 'custom-class'} ]
+    
+    
+    // const data=[]
+
+    const myData = [{angle: 1 ,color :1}, {angle: 5 , colo :2}, {angle: 2 , color :3}]
+
+    // const {data:session}=useSession()
 
 
-
-    const {data:session}=useSession()
+    console.log(topicwise);
+    
    
         
     
@@ -39,19 +45,19 @@ const Home:NextPage=()=>{
 
                     <div className='flex flex-row space-x-20 p-10'>
                     <div className='bg-[#f2f2f2] shadow-lg shadow-gray-400 flex flex-col rounded-[10px] h-[100px] w-[180px] justify-center items-center'>
-                        <h1 className='text-[32px] font-[500] text-[#4C4C4C]'>2000</h1>
+                        <h1 className='text-[32px] font-[500] text-[#4C4C4C]'>{articles.result.count}</h1>
                         <p className='text-[16px] font-[500] text-[#4C4C4C]'>Total Articles</p>
                     </div>
                     
                     
                     <div className='bg-[#f2f2f2] shadow-lg shadow-gray-400 flex flex-col rounded-[10px] h-[100px] w-[180px] justify-center items-center'>
-                        <h1 className='text-[32px] font-[500] text-[#4C4C4C]'>2000</h1>
+                        <h1 className='text-[32px] font-[500] text-[#4C4C4C]'>{authors.result.count}</h1>
                         <p className='text-[16px] font-[500] text-[#4C4C4C]'>Total Authors</p>
                     </div>
                     
                     
                     <div className='bg-[#f2f2f2] shadow-lg shadow-gray-400 flex flex-col rounded-[10px] h-[100px] w-[180px] justify-center items-center'>
-                        <h1 className='text-[32px] font-[500] text-[#4C4C4C]'>2000</h1>
+                        <h1 className='text-[32px] font-[500] text-[#4C4C4C]'>{topics.length}</h1>
                         <p className='text-[16px] font-[500] text-[#4C4C4C]'>Total Topics</p>
                     
                     </div>
@@ -59,26 +65,19 @@ const Home:NextPage=()=>{
                     </div>
 
                     <div className='flex justify-between pt-6 w-3/4'>
-                        <div className='flex w-full h-full border-r'>
                         
+                        
+                        
+                        <div className=' flex flex-col w-full h-full'>
+                        <h1 className='text-[20px] font-[500]'>Articles based on topic </h1>
                         <RadialChart
-                        data={myData}
-                        width={300}
-                        height={300} />
-
-                        <h1 className='text-[14px] font-[500]'>Authors based on topic</h1>
-                        
-                        </div>
-                        
-                        
-                        <div className=' flex w-full h-full'>
-                        <RadialChart
-                        data={myData}
+                        data={topicwise}
                         width={300}
                         height={300}
+                        showLabels = {true}
                         />
 
-                        <h1 className='text-[14px] font-[500]'>Articles based on topic </h1>
+                        
 
                         </div>
 
@@ -100,6 +99,51 @@ const Home:NextPage=()=>{
 
 }
 
+export async function getServerSideProps() {
+    try {
+        const response = await axios.get("http://localhost:4000/articles")
+        const res = await axios.get("http://localhost:4000/authors")
 
+        const uniqueTopics = []
+        const topicWise =[]
+
+        
+
+
+        response.data.result.rows.map((e)=>{
+
+            if(!uniqueTopics.includes(e.topic)){
+                uniqueTopics.push(e.topic)
+                topicWise.push({angle:1,label : e.topic})
+            }
+
+            else{
+                const index = uniqueTopics.indexOf(e.topic)
+                
+                
+                topicWise[index].angle=topicWise[index].angle+1
+            }
+        })
+
+        return {props:{ 
+                articles : response.data,
+                authors : res.data
+                ,topics : uniqueTopics,
+                topicwise : topicWise
+            }}
+        
+    }
+    catch(err){
+        console.log(err);
+        
+        return({
+            props : {
+                data : {}
+               }   }
+        )
+
+    }
+    
+}
 
 export default Home;
