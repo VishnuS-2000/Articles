a7dca97eb05cf235d3472c114bed8af7092cfc18
VishnuS-2000 <vishnusatish2000@gmail.com>
2022-06-26 23:59:07 +0530

    bug fix author link

--- pages/admin/dashboard/authors/view/[id].jsx
+++ pages/admin/dashboard/authors/view/[id].jsx
@@ -76,7 +76,7 @@ const AuthorView=({data}) => {
             {/* Topbar */}
             <div className='flex  items-center justify-between m-8'>
                 <div className='flex gap-8'>
-                    <Link href='#'>
+                    <Link href='/admin/dashboard/authors/?page=1'>
                         <button>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M19 12H5" stroke="#394867" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
